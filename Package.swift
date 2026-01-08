f5e04a0ffca23c60e70c870c5f6bbfe3b9562319
Quinn <coderquinn@gmail.com>
2026-01-09 07:30:16 +0800

    chores: update dependencies

--- Package.swift
+++ Package.swift
@@ -14,10 +14,8 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(path: "../ForgeBase"),
-        .package(path: "../ForgeLogKit"),
-//        .package(url: "https://github.com/CoderQuinn/ForgeBase.git", from: "0.0.1"),
-//        .package(url: "https://github.com/CoderQuinn/ForgeLogKit.git", from: "0.1.0"),
+        .package(url: "https://github.com/CoderQuinn/ForgeBase.git", from: "0.1.0"),
+        .package(url: "https://github.com/CoderQuinn/ForgeLogKit.git", from: "0.2.0"),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-nio-transport-services.git", from: "1.19.0"),
     ],
